eb5673d9ab93b29992e3944dfeed0968f2d72b99
Denilson Jhon <denilsontrillo@gmail.com>
2024-11-15 20:25:32 -0600

    Creacion de rutas completas

--- routes/web.php
+++ routes/web.php
@@ -1,7 +1,16 @@
 <?php
 
+use App\Http\Controllers\LoginController;
+use App\Http\Controllers\PasswordResetController;
 use Illuminate\Support\Facades\Route;
 
+// Ruta de inicio para login
 Route::get('/', function () {
-    return view('welcome');
+    return view('auth.login');
 });
+
+// Ruta de inicio para login
+Route::get('/login', [LoginController::class, 'showLoginForm'])->name('login.form');
+
+// Ruta para mostrar formulario
+Route::get('/forgot-password', [PasswordResetController::class, 'showForgotPasswordForm'])->name('forgot-password.form');
